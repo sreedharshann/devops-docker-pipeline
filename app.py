cfae96f01ba583b49f6416df91a109ae7107e5eb
Sree Dharshan K S <2023cs_sreedharshanks_d@nie.ac.in>
2026-05-19 11:47:10 +0530

    Updated Flask UI

--- app.py
+++ app.py
@@ -4,7 +4,59 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "DevOps Pipeline Running Successfully!"
+    return """
+    <html>
+        <head>
+            <title>DevOps CI/CD Project</title>
+            <style>
+                body {
+                    font-family: Arial, sans-serif;
+                    background-color: #0f172a;
+                    color: white;
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    height: 100vh;
+                    margin: 0;
+                }
+
+                .container {
+                    text-align: center;
+                    background: #1e293b;
+                    padding: 40px;
+                    border-radius: 15px;
+                    box-shadow: 0 0 20px rgba(0,0,0,0.4);
+                }
+
+                h1 {
+                    color: #38bdf8;
+                }
+
+                p {
+                    font-size: 18px;
+                }
+
+                .status {
+                    margin-top: 20px;
+                    color: #22c55e;
+                    font-weight: bold;
+                }
+            </style>
+        </head>
+
+        <body>
+            <div class="container">
+                <h1>🚀 DevOps CI/CD Pipeline</h1>
+
+                <p>Flask Application Successfully Deployed</p>
+
+                <div class="status">
+                    ✅ Jenkins + Docker Pipeline Running
+                </div>
+            </div>
+        </body>
+    </html>
+    """
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
